f2efaa415401c399424d6a73ff9d0b07b592dc02
Qassem Alsaffar <qas.alsaffar@gmail.com>
2022-10-19 19:52:11 +0300

    Add the relationships and passed the pytest

--- classes/models.py
+++ classes/models.py
@@ -7,6 +7,8 @@ class Course(models.Model):
 
 class Lecture(models.Model):
     name = models.CharField(max_length=30)
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="lectures")
 
     def __str__(self):
         return self.name
@@ -15,6 +17,8 @@ class Lecture(models.Model):
 class Slide(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lecture = models.OneToOneField(
+        Lecture, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.name
@@ -23,6 +27,8 @@ class Slide(models.Model):
 class Assignment(models.Model):
     name = models.CharField(max_length=30)
     link = models.URLField()
+    lecture = models.OneToOneField(
+        Lecture, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.name
@@ -30,6 +36,7 @@ class Assignment(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
+    courses = models.ManyToManyField(Course, related_name="tags")
 
     def __str__(self):
         return self.name
